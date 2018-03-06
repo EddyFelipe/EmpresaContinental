b8ccbef8ab619f4fb293b662c2833603e59abec0
Eddy Felipe <eddigzol@gmail.com>
2018-03-06 17:27:39 -0600

    Agrgue la linea de la conexion de la base datos

--- src/Clases/Conexion.java
+++ src/Clases/Conexion.java
@@ -1,14 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Clases;
 
-/**
- *
- * @author Hp
- */
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Conexion {
     
+    public Conexion(){}
+    
+    public void ConectarBaseDatos(){
+        try {
+            Connection con = DriverManager.getConnection("jdbc:mariadb://localhost:3306/nombre de la base datos", "root", "password");
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
