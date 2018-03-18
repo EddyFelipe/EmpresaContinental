b2ab68f67a3966799f38fe94f40e7dd77a91ce55
Ismar Miranda <ismarmiranda18@gmail.com>
2018-03-18 00:05:39 -0600

    He agregado clase proveedor

---
+++ src/Clases/Proveedor.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author ismar
+ */
+public class Proveedor {
+    public void insertar(String nombre,String empresa,String correo,String direccion, String telefono ){
+        
+    }
+    public void Modificar(int id,String nombre,String empresa,String correo,String direccion, String telefono){
+        
+    }
+}
